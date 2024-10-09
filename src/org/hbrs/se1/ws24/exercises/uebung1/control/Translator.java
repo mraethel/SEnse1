da3582f70c90e1649eb2204b6ee1c88fca8e5d7b
Maximilian Raethel <maximilian.raethel@smail.inf.h-brs.de>
2024-10-09 15:04:52 +0200

    u01

--- src/org/hbrs/se1/ws24/exercises/uebung1/control/Translator.java
+++ src/org/hbrs/se1/ws24/exercises/uebung1/control/Translator.java
@@ -10,14 +10,14 @@ package org.hbrs.se1.ws24.exercises.uebung1.control;
  */
 
 interface Translator {
-	
-	double version = 1.0; // Version des Interface
-	
-	/*
-	 * Uebersetzt eine numerische Zahl in eine String-basierte
-	 * Repraesentation gemaess der Spezifikation in der Aufgabe 1-2 
-	 */
-    String translateNumber(int number);
+  
+  double version = 1.0; // Version des Interface
+  
+  /*
+   * Uebersetzt eine numerische Zahl in eine String-basierte
+   * Repraesentation gemaess der Spezifikation in der Aufgabe 1-2 
+   */
+  String translateNumber (int number);
 
 } 
 
